72e053fd69233fc2630e5ef59ad17c031810fc6c
Kelly Whiting <kwhiting@gmail.com>
2018-02-12 13:34:45 -0800

    working keyboard translator

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-// console.log('Hello World');
-/*
-- demonstrate that you fully understand the problem
-- consider all edge-cases
-- simple solutions
-- clean, readable code
-- use meaningful variable names
-- choose appropriate abstractions
-- `node index.js` to run the file from the command prompt
-*/
+//`node index.js` to run the file from the command prompt
 
 var translateWithNewKeyboard = function(transform, paragraph) {
   //ignore capitalization
-  //transformations = string of characters: H, V, S+int
-  //edit the keyboard with all transformations in order
-  // then,
-  //return paragraph typed with new keyboard
   var keyboard = [['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
     ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']];
+  var keyboardHash = {};
+  for (var x = 0; x < keyboard.length; x++) {
+    for (var y = 0; y < keyboard[x].length; y++) {
+      keyboardHash[keyboard[x][y]] = [x, y];
+    }
+  }
 
-  //if H, rotate within same array - y axis
-  //if V, swap lines on x axis
-  //if S, algorithm to move index by value,
-  //if outside of bounds on right,
-  //then go to next x axis
-  //if outside of bounds on bottom, go to the top x = 0
-  //consider negative, goes to the bottom
-  if (!transform) {throw new Error ('need valid keyboard transformation order');}
+  if (!transform) {throw new Error ('need valid keyboard transformation orders');}
   if (transform.length < 1) {return paragraph;}
   var shiftStr = '';
   var shiftCount = 0;
@@ -55,6 +41,7 @@ var translateWithNewKeyboard = function(transform, paragraph) {
         shiftStr += transform[i];
         i++;
       }
+      i--;
       shiftCount = parseInt(shiftStr);
       var newKeyboard = [[],[],[],[]];
       for (var row = 0; row < keyboard.length; row++) {
@@ -64,7 +51,8 @@ var translateWithNewKeyboard = function(transform, paragraph) {
           } else {
             if (charIndex + shiftCount >= keyboard[row].length) {
               if (row + Math.floor((charIndex + shiftCount) / keyboard[row].length) < keyboard.length) {
-                newKeyboard[row + Math.floor((charIndex + shiftCount) / keyboard[row].length)][(charIndex + shiftCount) % keyboard[row].length] =
+                newKeyboard[row + Math.floor((charIndex + shiftCount) / keyboard[row].length)]
+                  [(charIndex + shiftCount) % keyboard[row].length] =
                   keyboard[row][charIndex];
               } else {
                 var newRow = (row + Math.floor((charIndex + shiftCount) / keyboard[row].length)) % keyboard.length;
@@ -75,7 +63,8 @@ var translateWithNewKeyboard = function(transform, paragraph) {
               }
             } else {
               // if charIndex + shiftCount < 0
-
+              newRow = (row + 3) % keyboard.length;
+              newKeyboard[newRow][keyboard[row].length + (charIndex + shiftCount)] = keyboard[row][charIndex];
             }
           }
         }
@@ -83,10 +72,32 @@ var translateWithNewKeyboard = function(transform, paragraph) {
       keyboard = newKeyboard;
     }
   }
-  console.log(keyboard);
+  paragraph = paragraph.toLowerCase();
+  var translatedContent = '';
+  var newLetter;
+  for (var char = 0; char < paragraph.length; char++) {
+    if (keyboardHash[paragraph[char]]) {
+      newLetter = keyboard[keyboardHash[paragraph[char]][0]][keyboardHash[paragraph[char]][1]];
+      translatedContent += newLetter;
+    } else {
+      translatedContent += paragraph[char];
+    }
+  }
+  console.log('The new keyboard is: ', keyboard);
+  console.log('The original paragraph was: ', paragraph);
+  console.log('The translated paragraph is: ', translatedContent);
 };
 // translateWithNewKeyboard('H', 'What a nice day it is today!');
 // translateWithNewKeyboard('V', 'What a nice day it is today!');
 // translateWithNewKeyboard('S20', 'What a nice day it is today!');
-translateWithNewKeyboard('S-1', 'What a nice day it is today!');
-//translateWithNewKeyboard('HHVS12VHVHS3', 'What a nice day it is today!');
+// translateWithNewKeyboard('S-2', 'What a nice day it is today!');
+// translateWithNewKeyboard('HVS1', 'What a nice day it is today!');
+// translateWithNewKeyboard('H', 'What a nice day it is today!');
+// translateWithNewKeyboard('HH', 'What a nice day it is today!');
+// translateWithNewKeyboard('HHV', 'What a nice day it is today!');
+// translateWithNewKeyboard('HHVS12', 'What a nice day it is today!');
+// translateWithNewKeyboard('HHVS12V', 'What a nice day it is today!');
+// translateWithNewKeyboard('HHVS12VH', 'What a nice day it is today!');
+// translateWithNewKeyboard('HHVS12VHV', 'What a nice day it is today!');
+// translateWithNewKeyboard('HHVS12VHVH', 'What a nice day it is today!');
+translateWithNewKeyboard('HHVS12VHVHS3', 'What a nice day it is today!');
