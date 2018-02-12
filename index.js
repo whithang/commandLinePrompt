f585753570dac31b7ed7997ac3fe8fb1cc9a4925
Kelly Whiting <kwhiting@gmail.com>
2018-02-11 22:54:38 -0800

    hello world working

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Hello World');
+/*
+- demonstrate that you fully understand the problem
+- consider all edge-cases
+- simple solutions
+- clean, readable code
+- use meaningful variable names
+- choose appropriate abstractions
+- `node index.js` to run the file from the command prompt
+*/
