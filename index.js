269be748eb9c801b63ff07828df802ba4e5d57bc
Kelly Whiting <kwhiting@gmail.com>
2018-02-13 13:45:14 -0800

    formatting changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var translateWithNewKeyboard = function(transform, paragraph) {
   //ignore capitalization
+  //update var to let/const
   var keyboard = [['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
@@ -13,78 +14,102 @@ var translateWithNewKeyboard = function(transform, paragraph) {
 
   if (!transform) {throw new Error ('need valid keyboard transformation orders');}
   if (transform.length < 1) {return paragraph;}
-  var shiftStr = '';
-  var shiftCount = 0;
-  var temp;
-  for (var i = 0; i < transform.length; i++) {
-    if (transform[i] === 'H') {
-      for (var h = 0; h < keyboard.length; h++) {
-        var rowLength = keyboard[h].length - 1;
-        for (var charIndex = 0; charIndex < keyboard[h].length / 2; charIndex++) {
-          temp = keyboard[h][charIndex];
-          keyboard[h][charIndex] = keyboard[h][rowLength - charIndex];
-          keyboard[h][rowLength - charIndex] = temp;
-        }
+  for (var transformIndex = 0; transformIndex < transform.length; transformIndex++) {
+    if (transform[transformIndex] === 'H') {
+      keyboard = horizontalKeyboard(keyboard);
+    } else if (transform[transformIndex] === 'V') {
+      keyboard = verticalKeyboard(keyboard);
+    } else if (transform[transformIndex] === 'S') {
+      var shiftStr = '';
+      var shiftCount = 0;
+      transformIndex++;
+      shiftStr = '';
+      while (transform[transformIndex] !== undefined && transform[transformIndex] !== 'H' &&
+        transform[transformIndex] !== 'V' && transform[transformIndex] !== 'S') {
+        shiftStr += transform[transformIndex];
+        transformIndex++;
       }
-    } else if (transform[i] === 'V') {
-      temp = keyboard[0];
-      keyboard[0] = keyboard[3];
-      keyboard[3] = temp;
-      temp = keyboard[1];
-      keyboard[1] = keyboard[2];
-      keyboard[2] = temp;
-    } else if (transform[i] === 'S') {
-      i++;
-      while (transform[i] !== undefined && transform[i] !== 'H' && transform[i] !== 'V' && transform[i] !== 'S') {
-        shiftStr += transform[i];
-        i++;
-      }
-      i--;
+      transformIndex--;
       shiftCount = parseInt(shiftStr);
-      var newKeyboard = [[],[],[],[]];
-      for (var row = 0; row < keyboard.length; row++) {
-        for (var charIndex = 0; charIndex < keyboard[row].length; charIndex++) {
-          if (charIndex + shiftCount < keyboard[row].length && charIndex + shiftCount >= 0) {
-            newKeyboard[row][charIndex + shiftCount] = keyboard[row][charIndex];
-          } else {
-            if (charIndex + shiftCount >= keyboard[row].length) {
-              if (row + Math.floor((charIndex + shiftCount) / keyboard[row].length) < keyboard.length) {
-                newKeyboard[row + Math.floor((charIndex + shiftCount) / keyboard[row].length)]
-                  [(charIndex + shiftCount) % keyboard[row].length] =
-                  keyboard[row][charIndex];
-              } else {
-                var newRow = (row + Math.floor((charIndex + shiftCount) / keyboard[row].length)) % keyboard.length;
-                if (newRow >= keyboard.length) {
-                  newRow = newRow % keyboard.length;
-                }
-                newKeyboard[newRow][(charIndex + shiftCount) % keyboard[row].length] = keyboard[row][charIndex];
-              }
-            } else {
-              // if charIndex + shiftCount < 0
-              newRow = (row + 3) % keyboard.length;
-              newKeyboard[newRow][keyboard[row].length + (charIndex + shiftCount)] = keyboard[row][charIndex];
-            }
-          }
-        }
-      }
-      keyboard = newKeyboard;
+      keyboard = shiftKeyboard(shiftCount, keyboard);
     }
   }
   paragraph = paragraph.toLowerCase();
-  var translatedContent = '';
-  var newLetter;
-  for (var char = 0; char < paragraph.length; char++) {
-    if (keyboardHash[paragraph[char]]) {
-      newLetter = keyboard[keyboardHash[paragraph[char]][0]][keyboardHash[paragraph[char]][1]];
-      translatedContent += newLetter;
-    } else {
-      translatedContent += paragraph[char];
-    }
-  }
+  var translatedContent = translateParagraph(paragraph);
+
   console.log('The new keyboard is: ', keyboard);
   console.log('The original paragraph was: ', paragraph);
   console.log('The translated paragraph is: ', translatedContent);
 };
+
+var horizontalKeyboard = function(keyboard) {
+  //could use reverse() function
+  var temp;
+  for (var row = 0; row < keyboard.length; row++) {
+    var rowLength = keyboard[row].length - 1;
+    for (var charIndex = 0; charIndex < keyboard[row].length / 2; charIndex++) {
+      temp = keyboard[row][charIndex];
+      keyboard[row][charIndex] = keyboard[row][rowLength - charIndex];
+      keyboard[row][rowLength - charIndex] = temp;
+    }
+  }
+  return keyboard;
+};
+
+var verticalKeyboard = function(keyboard) {
+  var temp = keyboard[0];
+  keyboard[0] = keyboard[3];
+  keyboard[3] = temp;
+  temp = keyboard[1];
+  keyboard[1] = keyboard[2];
+  keyboard[2] = temp;
+  return keyboard;
+};
+
+var shiftKeyboard = function(shiftCount, keyboard) {
+  //consider implementing concat and slice functions
+  var newKeyboard = [[],[],[],[]];
+  for (var row = 0; row < keyboard.length; row++) {
+    for (var charIndex = 0; charIndex < keyboard[row].length; charIndex++) {
+      if (charIndex + shiftCount < keyboard[row].length && charIndex + shiftCount >= 0) {
+        newKeyboard[row][charIndex + shiftCount] = keyboard[row][charIndex];
+      } else {
+        if (charIndex + shiftCount >= keyboard[row].length) {
+          if (row + Math.floor((charIndex + shiftCount) / keyboard[row].length) < keyboard.length) {
+            newKeyboard[row + Math.floor((charIndex + shiftCount) / keyboard[row].length)]
+              [(charIndex + shiftCount) % keyboard[row].length] =
+              keyboard[row][charIndex];
+          } else {
+            var newRow = (row + Math.floor((charIndex + shiftCount) / keyboard[row].length)) % keyboard.length;
+            if (newRow >= keyboard.length) {
+              newRow = newRow % keyboard.length;
+            }
+            newKeyboard[newRow][(charIndex + shiftCount) % keyboard[row].length] = keyboard[row][charIndex];
+          }
+        } else {
+          // if charIndex + shiftCount < 0
+          newRow = (row + 3) % keyboard.length;
+          newKeyboard[newRow][keyboard[row].length + (charIndex + shiftCount)] = keyboard[row][charIndex];
+        }
+      }
+    }
+  }
+  return newKeyboard;
+};
+
+var translateParagraph = function(paragraph){
+  var newContent = '';
+  var newLetter;
+  for (var char = 0; char < paragraph.length; char++) {
+    if (keyboardHash[paragraph[char]]) {
+      newLetter = keyboard[keyboardHash[paragraph[char]][0]][keyboardHash[paragraph[char]][1]];
+      newContent += newLetter;
+    } else {
+      newContent += paragraph[char];
+    }
+  }
+};
+
 // translateWithNewKeyboard('H', 'What a nice day it is today!');
 // translateWithNewKeyboard('V', 'What a nice day it is today!');
 // translateWithNewKeyboard('S20', 'What a nice day it is today!');
